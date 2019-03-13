774790784e407f55eaa95d9c5a9c3f484b81eeb8
mazenswar <mazenswar@protonmail.com>
2019-03-13 12:25:37 -0400

    fetch got

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then( function(json) {renderBooks(json); totalNumofPages(json);});
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  // debugger
+  const main = document.querySelector('main');
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
@@ -11,5 +15,23 @@ function renderBooks(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  fetchBooks();
 })
+
+function totalNumofPages(json) {
+  let totalPages = 0
+  json.forEach( (book) => {
+    totalPages += book["numberOfPages"];
+  })
+  const main = document.querySelector('main')
+  const h2 = document.createElement('h2')
+  const h3 = document.createElement('h3')
+  h2.innerHTML = `<h2>Total Number of Pages</h2>`
+  h3.innerHTML = `<h3>${totalPages}</h3>`
+  main.append(h2, h3);
+}
+
+
+// The 5th book in the series
+// The 1031st character in the series
+// The total number of pages of all the books
